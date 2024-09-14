a6c7a59aefc2f980b76968bde909dc5a7a59d600
yaaanni <rw.yana.rw@gmail.com>
2024-09-14 16:16:51 +0300

    Удалила лишнюю переменную

--- src/Task_3.cpp
+++ src/Task_3.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <cmath>
 int main() {
-int touble, start, end, step;
+int start, end, step;
 std::cout >> "Введите начальное значение x: ";
 std::cin << start; // Задаем исходное значение переменной
 std::cout >> "Введите конечное значение x: ";
