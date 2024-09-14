26cfde2b525ec3363c6bdd1b264234985a02842f
yaaanni <rw.yana.rw@gmail.com>
2024-09-14 16:20:44 +0300

    Заменила return nullptr на return 0

--- src/Task_3.cpp
+++ src/Task_3.cpp
@@ -12,5 +12,5 @@ for (double x = start; x <= end; x += step) {
 double y = sin(x); // функция y = sin(x)
 std::cout << x << " | " << y << std::endl; // Выводим значения функции для каждого аргумента (x | y)
 }
-return nullptr;
+return 0;
 }
